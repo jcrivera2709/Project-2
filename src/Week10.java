941dfdfb63573b3a58862c339f455618d6d8d5be
jcrivera2709 <jcrivera2709@7127R4S4.STUDENT.ad.fgcu.edu>
2018-11-29 09:58:18 -0500

    Array

--- src/Week10.java
+++ src/Week10.java
@@ -2,18 +2,21 @@ import java.util.Scanner;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.util.Random;
+
 
 public class Week10 {
+  public static void main(String[] args) throws InterruptedException {
 
-  // 2D integer array using a for loop
-  int[][] board = new int[3][3];
-
-  for( int i = 0;i<board.length;i++)
-  {
-    for (int j = 0; j < board[i].length; j++) {
-      board[i][j] = i + j;
+    //Scanner scan = new Scanner(System.in);
+    int arr [] = {};
+    int i;
+    for (i=0; i<10; i++) {
+      int rand = (int) Math.round(Math.random() * 100);
+      arr[i] = rand;
     }
+    
+    System.out.println(arr);
+    
   }
 }
 
