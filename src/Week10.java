b05c3ce96860fadc8a92de9db05465b8a474f9a2
jcrivera2709 <jcrivera2709@7127R4S4.STUDENT.ad.fgcu.edu>
2018-11-29 10:17:05 -0500

    Hello

--- src/Week10.java
+++ src/Week10.java
@@ -7,16 +7,21 @@ import javax.swing.*;
 public class Week10 {
   public static void main(String[] args) throws InterruptedException {
 
-    //Scanner scan = new Scanner(System.in);
-    int arr [] = {};
-    int i;
-    for (i=0; i<10; i++) {
-      int rand = (int) Math.round(Math.random() * 100);
+    // Scanner scan = new Scanner(System.in);
+    int[] arr = new int[10];
+    for (int i = 0; i < 10; i++) {
+      int rand = (int) Math.round(Math.random() * 10);
       arr[i] = rand;
     }
     
-    System.out.println(arr);
+   
+    for(int j=0;j <= arr.length; j++) {
+      int x;
+      x = Math.min(arr[j],arr[j+1]); 
+      System.out.println(x);
+    }
     
+
   }
 }
 
