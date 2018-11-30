7890697b30b7c0507d63dd8943cb66a0193f12a4
DemonTailes <DemonTailes@172.26.209.173>
2018-11-30 10:34:57 -0500

    Project Submission 2

--- src/Week11.java
+++ src/Week11.java
@@ -1,18 +1,31 @@
 import java.util.Scanner;
 
 public class Week11 {
-  public static void w11(String[] args) throws InterruptedException {
+  public static void main(String[] args) throws InterruptedException {
     Scanner scan = new Scanner(System.in);
     try {
       System.out.println("Enter a number");
       int num1 = scan.nextInt();
       System.out.println("Enter another number");
       int num2 = scan.nextInt();
-      System.out.println("Result = " + num1 / num2);
+      System.out.println("Answer : " + num1 / num2);
     } catch (Exception ex) {
       System.out.println(ex);
+    }  
+    
+    System.out.println();
+    boolean correct = false;
+    int num = 0;
+    System.out.println("Enter a number");
+    while (correct == false) {
+      try {
+        num = scan.nextInt();
+        correct = true;
+      } catch (Exception ex) {
+        System.out.println("Please enter an integer");
+        scan.nextLine();
+      }
     }
-    scan.close();
+    System.out.println("Thank you!");
   }
-  
 }
